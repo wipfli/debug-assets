57968c7d5b1652e7495108bf82afa10ab1e4351c
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-05-15 08:52:21 +0200

    Add weather layers example

---
+++ weatherlayers/merge.sh
@@ -0,0 +1,18 @@
+rm U_swiss_fixed.tif
+rm V_swiss_fixed.tif
+rm magnitude_swiss_fixed.tif
+
+gdalwarp -overwrite -tr 0.013158586806079 -0.013377209829348 U_swiss.tif U_swiss_fixed.tif
+gdalwarp -overwrite -tr 0.013158586806079 -0.013377209829348 V_swiss.tif V_swiss_fixed.tif
+
+gdal_calc.py --calc='sqrt(A * A + B * B)' -A U_swiss_fixed.tif --A_band=1 -B V_swiss_fixed.tif --B_band=1 --outfile wind_magnitude_fixed.tif
+
+gdalbuildvrt -separate wind.vrt wind_magnitude_fixed.tif U_swiss_fixed.tif V_swiss_fixed.tif
+
+gdal_translate -ot Byte -scale -128 127 0 255 wind.vrt wind.png
+
+rm U_swiss_fixed.tif
+rm V_swiss_fixed.tif
+rm wind.png.aux.xml
+rm wind.vrt
+rm wind_magnitude_fixed.tif
