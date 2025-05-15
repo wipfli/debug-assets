1fc8d416fce7d675da565e0cc4c5225e4486368a
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2025-05-15 09:14:54 +0200

    Fix

--- weatherlayers/merge.sh
+++ weatherlayers/merge.sh
@@ -7,12 +7,10 @@ gdalwarp -overwrite -tr 0.013158586806079 -0.013377209829348 V_swiss.tif V_swiss
 
 gdal_calc.py --calc='sqrt(A * A + B * B)' -A U_swiss_fixed.tif --A_band=1 -B V_swiss_fixed.tif --B_band=1 --outfile wind_magnitude_fixed.tif
 
-gdalbuildvrt -separate wind.vrt wind_magnitude_fixed.tif U_swiss_fixed.tif V_swiss_fixed.tif
+gdal_calc.py --calc='(-1) * A' -A U_swiss_fixed.tif --A_band=1 --outfile U_swiss_fixed_inv.tif
+gdal_calc.py --calc='(-1) * A' -A V_swiss_fixed.tif --A_band=1 --outfile V_swiss_fixed_inv.tif
 
-gdal_translate -ot Byte -scale -128 127 0 255 wind.vrt wind.png
+gdalbuildvrt -separate wind.vrt wind_magnitude_fixed.tif U_swiss_fixed_inv.tif V_swiss_fixed.tif
 
-rm U_swiss_fixed.tif
-rm V_swiss_fixed.tif
-rm wind.png.aux.xml
-rm wind.vrt
-rm wind_magnitude_fixed.tif
+# gdal_translate -ot Byte -scale -128 127 0 255 wind.vrt wind.png
+gdal_translate -ot Byte -scale 128 -127 0 255 wind.vrt wind.png
